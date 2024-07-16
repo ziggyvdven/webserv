49f89d7d46a5fd0298fea8c13a490937728e23b7
Karim Mehour <kmehour@c1r8p6.42quebec.com>
2024-07-16 19:05:41 -0400

    Rewrite test_main

--- unit_tests/test_main.cpp
+++ unit_tests/test_main.cpp
@@ -2,6 +2,8 @@
 #include "../includes/utils.hpp"
 #include <iostream>
 
+#include "../includes/HttpRequest.hpp"
+
 
 void test_split(std::string str, std::string delim, unsigned int expected_size)
 {
@@ -17,14 +19,28 @@ void test_split(std::string str, std::string delim, unsigned int expected_size)
 		for(std::vector<std::string>::iterator it = list.begin(); it != list.end(); ++it)
 			std::cout << *it << std::endl;
 	}
-	
 }
 
-int main(void)
+void test_split_batch()
 {
 	test_split("", "", 0);
 	test_split("hello", "", 1);
 	test_split("Hello", " ", 1);
 	test_split("Hello", "l", 2);
+}
+
+int main(void)
+{
+	std::string http_str = \
+	"POST /cgi-bin/echo HTTP/1.1\r\n"
+	"\r\n"
+	"<This is the full body content>";
+
+	std::cout << "Parsing stuff" << std::endl;
+	HttpRequest req(http_str);
+
+	std::cout << std::boolalpha << req.isValid() << std::endl;
+	std::cout << std::boolalpha << req.body() << std::endl;
+
 
 }

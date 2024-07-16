5c77735608aa9cc8fddf8f728105c205ce9ba4a3
Karim Mehour <kmehour@c1r8p6.42quebec.com>
2024-07-16 18:56:11 -0400

    Update utils.hpp

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <vector>
 
 std::string& trim(std::string& s);
 std::string& ltrim(std::string& s, const char* t);
@@ -6,3 +6,4 @@ std::string& rtrim(std::string& s, const char* t);
 
 void printStringWithNonPrintables(const std::string& str);
 void toLowerCase(std::string & str);
+std::vector<std::string> ft_split(std::string str, std::string const &delim);
